0b86ed1375dd02cba1224c5b227b09dd0a37d6d9
saman <samanobeydzadeh@gmail.com>
2023-09-16 01:39:53 +0330

    solve q4

--- 4/code.sql
+++
@@ -1,8 +0,0 @@
--- Section1
-    create index created_at_index
-    on orders (created_at, total);
--- Section2
-    create index index_name
-    on orders (user_id, created_at, total);
--- Section3
-    Your third query here
